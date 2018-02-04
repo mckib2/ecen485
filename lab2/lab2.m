208be4d224aa85a1352b37c2a7540f87ef6230fc
mckib2 <nicholas.bgp@gmail.com>
2018-02-03 17:17:20 -0700

    Added lab2 updates that make binary pam work.  This is basically Lab 1.

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -10,37 +10,60 @@ close all;
 sig = [ 1 0 0 1 ];
 b = .25*ones(1,16);
 N = 16;
+M = 2;
 
-r = modulater(sig,@binary_lut,b,N);
-[ a,x ] = demodulater(r,b,2,16);
+r = modulator(sig,@binary_lut,b,N);
+[ ~,~,x ] = demodulator(r,b,M,N);
 
 figure(1);
 plot(r); hold on; plot(x);
 title('demodulator input and the matched filter output');
 
+load('bb2data.mat');
+r = bb2data(2,:);
+[ s,~,x ] = demodulator(r,b,M,N);
 
+figure(2);
+plot(r); hold on; plot(x);
 
+addpath('../lab0/','-end');
+m2ascii(s,M);
+
+%% Now try out 8-ary PAM
+sig = [ 1 0 0 1 ];
+b = .25*ones(1,16);
+N = 16;
+M = 2;
 
 %% 8-ary look up table
-function [ out ] = eight_lut(in)
-    out = in;
-    out
+function [ out ] = eight_lut(in,~)
+    if nargin < 2
+        % symbol to A
+    else
+        % A to symbol
+    end
 end
 
 %% Binary look up table
-function [ out ] = binary_lut(in)
-    out = in;
-    out(out == 0) = -1;
+function [ out ] = binary_lut(in,~)
+    if nargin < 2
+        % symbol to A
+        out = in;
+        out(out == 0) = -1;
+    else
+        % A to symbol
+        out = double(in >= 0);
+    end
 end
 
 %% Modulator
-function [ r ] = modulater(s,lut,b,N)
+function [ r ] = modulator(s,lut,b,N)
     x = upsample(lut(s),N);
     r = filter(b,1,x);
 end
 
 %% Demodulator
-function [ a,x ] = demodulater(r,b,M,N)
+function [ s,a,x ] = demodulator(r,b,M,N)
 
     % Matched filter
     x = filter(b,1,[0 r]);
@@ -55,5 +78,6 @@ function [ a,x ] = demodulater(r,b,M,N)
         test_sig = d - xk(ii);
         [ ~,k ] = min(abs(test_sig));
         a(ii) = d(k);
+        s(ii) = binary_lut(a(ii),1);
     end
 end
