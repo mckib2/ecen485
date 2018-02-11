72668e5313c06fd00c7ba3b1ba16e7505663d941
mckib2 <nicholas.bgp@gmail.com>
2018-02-11 02:44:41 -0700

    Misc

--- pam/demodulator.m
+++ pam/demodulator.m
@@ -2,10 +2,10 @@
 function [ s,a,x,xk ] = demodulator(r,b,lut,N)
 
     % Matched filtering, spit a zero out front to make everything work
-    x = filter(fliplr(b),1,[ 0 r ]);
+    x = filter(fliplr(b),1,[ zeros([ size(r,1) 1 ]) r ]);
 
     % Downsample
-    xk = downsample(x(N:end),N);
+    xk = downsample(x(:,N:end).',N);
     
     % Make a decision
     d = cell2mat(keys(lut.reverse)); % amplitudes
